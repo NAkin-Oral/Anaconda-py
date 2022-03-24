3f19f2b5893484cef73c060c12fe40fd57f24478
nao <nurettinakinoral@gmail.com>
2022-03-24 18:31:45 +0300

    We started using github

---
+++ hello.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+""" this is my first module & script """
+
+def my_func1(x):
+    return print(x**2)
+
+def my_func2(y):
+    return print(*y)
+
+if __name__ == '__main__':  # output-generating statements are here
+    print('hello') 
+    my_func1(3)
+    my_func2("clarusway")
+
---
+++ my_module.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+def my_function_1(a, b):
+    area = a*b
+    return area
+def my_function_2(a, b):
+    hypotenuse = (a**2 + b**2)**0.5
+    return hypotenuse
+
